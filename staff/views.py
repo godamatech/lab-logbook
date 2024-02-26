61830c20ebc3cf68b6f653d3c3cb3f2e82d34548
Muhammad Muhammad Inuwa <mminuwaali@gmail.com>
2024-02-26 04:23:02 +0100

    Update staff and student views to use new models

--- staff/views.py
+++ staff/views.py
@@ -1,9 +1,15 @@
 from . import models
+from student.models import Record
 from django.shortcuts import render
 
 
 def index_view(request):
-    return render(request, "staff/index.html")
+    records = Record.objects.all()
+    students = models.User.objects.filter(groups__name="student")
+
+    context = {"records": records, "students": students}
+    return render(request, "staff/index.html", context)
+
 
 def detail_view(request, id):
     return render(request, "staff/detail.html")
